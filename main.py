773d7de328e825c6d334a185c653a74eaa851fb1
coli <kkwoo001021@naver.com>
2025-09-05 11:32:31 +0900

    feat: request body 작성

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 
 class ModelName(str, Enum):
@@ -7,17 +8,20 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+
 app = FastAPI()
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
-    if(model_name is ModelName.alexnet):
-        return {"model_name" : model_name, "message" : "Deep Learning FTW!"}
+    if (model_name is ModelName.alexnet):
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
     if (model_name.value == "lenet"):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
 
 @app.get("/{name}")
 def root(name: str):
@@ -28,14 +32,26 @@ def root(name: str):
 def home(name: int):
     return {"name": name}
 
+
 # 쿼리 스트링 예제
-fake_items_db = [{"item_name" : "Foo"}, {"item_name", "Bar"}, {"item_name", "Baz"}]
+fake_items_db = [{"item_name": "Foo"}, {"item_name", "Bar"}, {"item_name", "Baz"}]
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, page: int = 0, size: int = 10):
-    return fake_items_db[page: page+size]
+    return fake_items_db[page: page + size]
+
 
 # post 예제
 @app.post("/")
-def home_post(msg:str):
+def home_post(msg: str):
     return {"Hello": "POST", "msg": msg}
+
+
+class DataInput(BaseModel):
+    name: str
+
+
+@app.post("/")
+def home_post(data_request: DataInput):
+    return {"Hello": "POST", "msg": data_request.name}
